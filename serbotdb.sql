7d56d5cbec20beddc6e076eb39844fbcf4760f34
D0L0S <ricklwebb@gmail.com>
2015-05-06 13:29:28 +0100

    Update serbotdb.sql

--- serbotdb.sql
+++ serbotdb.sql
@@ -3,4 +3,5 @@
 -------------------------------------------------------------------
 CREATE DATABASE serbot;
 USE serbot;
-CREATE TABLE clients (id INT, name VARCHAR(20), ip_address VARCHAR(30), os VARCHAR(30));
+CREATE TABLE clients (id INT, name VARCHAR(20), ip_address VARCHAR(30), os VARCHAR(30), status VARCHAR(30));
+CREATE TABLE users (id INT, first_name VARCHAR(20), last_name VARCHAR(20), email VARCHAR(30), client_id INT(10));
