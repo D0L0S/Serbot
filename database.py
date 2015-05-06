efe4c7448d95268c55bb834d8d78232b51914592
D0L0S <ricklwebb@gmail.com>
2015-05-06 13:35:52 +0100

    Create database.py

---
+++ database.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+
+class Database:
+
+	def __init__(self):
+		user='root'
+		host='127.0.0.1'
+		database='serbot'
+		self.cnx = mysql.connector.connect(user=self.user, host=self.host, database=self.database)
+
+	def query(self, q):
+		cursor = self.cnx.cursor()
+		cursor.execute(q)
+		columns = tuple( [d[0].decode('utf8') for d in cursor.description] )
+		result = []
+		for row in cursor:
+			result.append(dict(zip(columns, row)))
+		return result
+
+	def __del__(self):
+		self.cnx.close()
+
+
+if __name__=="__main__":
+	sys.stdout = Logger("Database.log")
+	sys.stderr = Logger("DBError.log")
+	db=Database()
