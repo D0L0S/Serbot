587c49112e53fad46b99c2cf7194713f26ad71b4
D0L0S <ricklwebb@gmail.com>
2015-05-06 13:17:37 +0100

    Create serbotdb.sql

---
+++ serbotdb.sql
@@ -0,0 +1,6 @@
+-------------------------------------------------------------------
+-- Serbot DB v0.1                                                --
+-------------------------------------------------------------------
+CREATE DATABASE serbot;
+USE serbot;
+CREATE TABLE clients (id INT, name VARCHAR(20), ip_address VARCHAR(30), os VARCHAR(30));
